ce3f332b42858534579916b947d85ad1d33b34e4
pxsa <paghsis@gmail.com>
2023-11-28 20:04:15 +0330

    recode guessing number game

--- projects/guessing_number_game/human.py
+++ projects/guessing_number_game/human.py
@@ -1,40 +1,22 @@
 import random
 
-print("Welcome to guessing number game.")
+random_number = random.randint(1, 10)
+chances = 5
 
+while chances > 0:
+    user_number = int(input("Enter a number(1/10): "))
 
-while True:
-    want_to_play = input("\nAre you ready to play? (y/n) ")
-    if want_to_play == "n":
-        print("Bye...")
+    chances -= 1
+
+    if user_number == random_number:
+        print("you win the game.")
         break
 
-    elif want_to_play == "y":
+    if chances == 0:
+        print(f"sorry, you lose the game. the actual number was {random_number}")
 
-        start = int(input("Enter the start number: "))
-        end = int(input("Enter the end number: "))
-        n = int(input("Enter the number of chances: "))
-        number = random.randint(start, end)
+    elif user_number < random_number:
+        print(f"your number is too small. you only have {chances} chances.")
 
-        while n >= 1:
-            a = int(input("pick a number: "))
-
-            n = n - 1
-
-            if a == number:
-                print("congradulation, you won the game ;)")
-                break
-
-            if n == 0:
-                continue
-
-            if a > number:
-                print(f"your number is too high, you only have #{n} chances.")
-            else:
-                print(f"your number is too small, you only have #{n} chances.")
-
-        else:
-            print("Maybe it's not your day ;(")
-            print(f"The actuall number is: {number}")
-    else:
-        print("Please enter a valid input")
+    elif user_number > random_number:
+        print(f"your number is too large. you only have {chances} chances.")
